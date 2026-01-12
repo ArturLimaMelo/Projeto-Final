4ca0b42dd8efe12af9ed621cdf5e08b82d90c75d
NEGRESC0 <156157000+NEGRESC0@users.noreply.github.com>
2026-01-12 11:01:23 -0300

    sign in required content fix but name

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
-import './styles/global.css';
+import "./styles/global.css";
 import "./styles/themes.css";
 import { Route, Routes } from "react-router";
 
 import { Header } from "./components/Header.jsx";
-import Landing from './screens/Landing/Landing.jsx';
-import Footer from './components/Footer.jsx';
+import Landing from "./screens/Landing/Landing.jsx";
+import Footer from "./components/Footer.jsx";
 import Login from "./screens/Login/Login.jsx";
 import SignIn from "./screens/SignIn/SignIn.jsx";
 import Shop from "./screens/Shop/Shop.jsx";
-import { SessionProvider } from './context/SessionContext';
+import { SessionProvider } from "./context/SessionContext";
 
 export default function App() {
   return (
     <>
-    <SessionProvider>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/shop" element={<Shop />} />
-      </Routes>
-      <Footer />
-    </SessionProvider>
+      <SessionProvider>
+        <div className="app-container">
+          <Header />
+
+          <div className="app-content">
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signin" element={<SignIn />} />
+              <Route path="/shop" element={<Shop />} />
+            </Routes>
+          </div>
+
+          <Footer />
+        </div>
+      </SessionProvider>
     </>
   );
 }
--- src/screens/SignIn/SignIn.jsx
+++ src/screens/SignIn/SignIn.jsx
@@ -1,22 +1,21 @@
 import styles from "./SignIn.module.css";
-import { Field } from '@base-ui/react/field';
-import { Fieldset } from '@base-ui/react/fieldset';
+import { Field } from "@base-ui/react/field";
+import { Fieldset } from "@base-ui/react/fieldset";
 import { Form } from "@base-ui/react/form";
-import { Button } from '@base-ui/react/button';
-import Checkbox from '@mui/joy/Checkbox';
+import { Button } from "@base-ui/react/button";
+import Checkbox from "@mui/joy/Checkbox";
 import { Link } from "react-router";
+import { toast, Bounce } from "react-toastify";
 import { useContext, useState, useEffect } from "react";
 import { SessionContext } from "../../context/SessionContext";
 import { EyeIcon, EyeOffIcon } from "lucide-react";
 
-
-
 export default function SignIn() {
+  const { session, handleSignUp, sessionMessage, sessionError } =
+    useContext(SessionContext);
 
-  const { session, handleSignUp, sessionMessage, sessionError } = useContext(SessionContext);
+  const [showPassword, setShowPassword] = useState(false);
 
-  const [ showPassword, setShowPassword ] = useState(false);
-  
   const handleTogglePassword = () => setShowPassword((show) => !show);
 
   const [errors, setErrors] = useState({});
@@ -26,8 +25,64 @@ export default function SignIn() {
     password: "",
     confirmPassword: "",
     username: "",
-    name: ""
+    name: "",
   });
+
+  useEffect(() => {
+    if (sessionMessage) {
+      toast.success(sessionMessage, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        progress: undefined,
+        style: { fontSize: "1.5rem" },
+        theme: localStorage.getItem("theme"),
+        transition: Bounce,
+      });
+    } else {
+      toast.error(sessionError, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        progress: undefined,
+        style: { fontSize: "1.5rem" },
+        theme: localStorage.getItem("theme"),
+        transition: Bounce,
+      });
+    }
+  }, [sessionMessage, sessionError]);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const newErrors = {};
+    if (!formValues.name) newErrors.name = "Nome completo is required";
+
+    if (!formValues.username) newErrors.username = "Username is required";
+
+    if (!formValues.email) newErrors.email = "Email is required";
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (formValues.email && !emailRegex.test(formValues.email)) {
+      newErrors.email = "Invalid email format";
+    }
+
+    if (!formValues.password) newErrors.password = "Password is required";
+
+    if (!formValues.confirmPassword)
+      newErrors.confirmPassword = "Confirm Password is required";
+
+    if (formValues.password !== formValues.confirmPassword)
+      newErrors.confirmPassword = "Passwords do not match";
+
+    setErrors(newErrors);
+    if (Object.keys(newErrors).length > 0) return;
+
+    handleSignUp(formValues.email, formValues.password, formValues.username);
+  }
 
   function handleInputChange(e) {
     const { name, value } = e.target;
@@ -35,83 +90,108 @@ export default function SignIn() {
       ...prev,
       [name]: value,
     }));
-    console.log(value)
   }
 
-    async function handleSubmit(e) {
-      e.preventDefault();
-
-    const newErrors = {};
-      if (!formValues.name) newErrors.name = "Nome completo is required";
-      if (!formValues.username) newErrors.username = "Username is required";
-      if (!formValues.email) newErrors.email = "Email is required";
-      if (!formValues.password) newErrors.password = "Password is required";
-      if (!formValues.confirmPassword)
-        newErrors.confirmPassword = "Confirm Password is required";
-      if (formValues.password !== formValues.confirmPassword)
-        newErrors.confirmPassword = "Passwords do not match";
-    
-    setErrors(newErrors);
-    if (Object.keys(newErrors).length > 0) {
-      
-      console.log("Validation errors:", errors);
-      return;
-    }
-  
-  
-    handleSignUp(formValues.email, formValues.password, formValues.username);
-  }
   return (
     <div className={styles.container}>
-      <Form onSubmit={handleSubmit}>
-      <Fieldset.Root className={styles.Fieldset}>
-      <Fieldset.Legend className={styles.Legend}>Cadastrar-se</Fieldset.Legend>
+      <Form onSubmit={handleSubmit} errors={errors} onClearErrors={setErrors}>
+        <Fieldset.Root className={styles.Fieldset}>
+          <Fieldset.Legend className={styles.Legend}>
+            Cadastrar-se
+          </Fieldset.Legend>
 
-      <Field.Root className={styles.Field} error={errors.name}>
-        <Field.Label className={styles.Label}>Nome completo</Field.Label>
-        <Field.Control name="name" placeholder="Seu nome" className={styles.Input} onChange={handleInputChange} value={formValues.name} />
-        <Field.Error className={styles.Error}>{errors.name}</Field.Error>
-      </Field.Root>
+          <Field.Root className={styles.Field} error={errors.name}>
+            <Field.Label className={styles.Label}>Nome completo</Field.Label>
+            <Field.Control
+              type="name"
+              name="name"
+              required
+              value={formValues.name}
+              onChange={handleInputChange}
+              placeholder="Seu nome"
+              className={styles.Input}
+            />
+            <Field.Error className={styles.Error}>{errors.name}</Field.Error>
+          </Field.Root>
 
-      <Field.Root className={styles.Field} error={errors.username}>
-        <Field.Label className={styles.Label}>Nome de usuário</Field.Label>
-        <Field.Control name="username" placeholder="Empreendedor 123" className={styles.Input} onChange={handleInputChange} value={formValues.username} />
-        <Field.Error className={styles.Error} value={errors.username}/>
-      </Field.Root>
+          <Field.Root name="username" className={styles.Field}>
+            <Field.Label className={styles.Label}>Nome de usuário</Field.Label>
+            <Field.Control
+              type="username"
+              name="username"
+              required
+              value={formValues.username}
+              onChange={handleInputChange}
+              placeholder="Empreendedor 123"
+              className={styles.Input}
+            />
+            <Field.Error className={styles.Error} />
+          </Field.Root>
 
-      <Field.Root className={styles.Field} error={errors.email}>
-        <Field.Label className={styles.Label}>Email</Field.Label> 
-        <Field.Control name="email" placeholder="sounmx@exemplo.com" className={styles.Input} onChange={handleInputChange} value={formValues.email} />
-        <Field.Error className={styles.Error} value={errors.email}/>
-      </Field.Root>
+          <Field.Root name="email" className={styles.Field}>
+            <Field.Label className={styles.Label}>Email</Field.Label>
+            <Field.Control
+              type="email"
+              name="email"
+              required
+              value={formValues.email}
+              placeholder="sounmx@exemplo.com"
+              className={styles.Input}
+              onChange={handleInputChange}
+            />
+            <Field.Error className={styles.Error} />
+          </Field.Root>
 
-      <Field.Root className={styles.Field} error={errors.password}>
-        <Field.Label className={styles.Label}>Senha</Field.Label>
-
-        <Field.Control name="password" type={showPassword ? "text" : "password"} placeholder="amoempreender123" className={styles.Input} onChange={handleInputChange} value={formValues.password} />
-        <button
+          <Field.Root name="password" className={styles.Field}>
+            <Field.Label className={styles.Label}>Senha</Field.Label>
+            <Field.Control
+              name="password"
+              type={showPassword ? "text" : "password"}
+              required
+              value={formValues.password}
+              onChange={handleInputChange}
+              placeholder="amoempreender123"
+              className={styles.Input}
+            />
+            <button
               type="button"
               className={styles.passwordToggle}
               onClick={handleTogglePassword}
-              aria-label={showPassword}
+              aria-label={showPassword ? "Ocultar senha" : "Mostrar senha"}
+              title={showPassword ? "Ocultar senha" : "Mostrar senha"}
               aria-controls="password"
-        >{showPassword ? <EyeOffIcon /> : <EyeIcon />}</button>
-        <Field.Error className={styles.Error}>{errors.password}</Field.Error>
-      </Field.Root>
+            >
+              {showPassword ? <EyeOffIcon /> : <EyeIcon />}
+            </button>
+            <Field.Error className={styles.Error} />
+          </Field.Root>
 
-      <Field.Root className={styles.Field} error={errors.confirmPassword}>
-        <Field.Label className={styles.Label}>Confirme sua senha</Field.Label>
-        <Field.Control name="confirmPassword" type={showPassword ? "text" : "password"} placeholder="amoempreender123" className={styles.Input} onChange={handleInputChange} value={formValues.confirmPassword} />
-        <Field.Error className={styles.Error}>{errors.confirmPassword}</Field.Error>
-      </Field.Root>
-    </Fieldset.Root>
+          <Field.Root name="confirmPassword" className={styles.Field}>
+            <Field.Label className={styles.Label}>
+              Confirme sua senha
+            </Field.Label>
+            <Field.Control
+              name="confirmPassword"
+              type={showPassword ? "text" : "password"}
+              required
+              value={formValues.confirmPassword}
+              onChange={handleInputChange}
+              placeholder="amoempreender123"
+              className={styles.Input}
+            />
+            <Field.Error className={styles.Error} />
+          </Field.Root>
+        </Fieldset.Root>
 
-    <Checkbox label="Deseja receber ofertas no seu email?" color="neutral" />
+        <Checkbox
+          label="Deseja receber ofertas no seu email?"
+          color="neutral"
+        />
 
-    <Button type="submit" className={styles.Button}>
-        Submit
-      </Button>
-    </Form>
+        <Button type="submit" className={styles.Button}>
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }

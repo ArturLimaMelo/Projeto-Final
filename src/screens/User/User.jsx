0df4112566ef4f813b2e722dd71f6cdce92a7e87
NEGRESC0 <156157000+NEGRESC0@users.noreply.github.com>
2026-01-15 16:39:32 -0300

    create store on User.jsx

--- src/screens/User/User.jsx
+++ src/screens/User/User.jsx
@@ -4,10 +4,16 @@ import { useContext, useEffect, useState } from "react";
 import { supabase } from "../../utils/supabase";
 import { Link } from "react-router";
 
+import { Field } from "@base-ui/react/field";
+import { Form } from "@base-ui/react/form";
+import { Button } from "@base-ui/react/button";
+
 export default function User() {
   const { session } = useContext(SessionContext);
   const [userData, setUserData] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [errors, setErrors] = useState({});
+  const [buildStore, setBuildStore] = useState(false);
 
   async function fetchUserData() {
     if (!session?.user) return;
@@ -29,9 +35,14 @@ export default function User() {
 
   useEffect(() => {
     fetchUserData();
+    console.log("Build Store:", buildStore);
   }, [session]);
 
   if (loading) return <p>Loading user data...</p>;
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+  }
 
   return (
     <>
@@ -62,11 +73,85 @@ export default function User() {
           </div>
         </div>
         <div className={styles.storeSide}>
-          <div className={styles.storePanel}>
+          <div
+            className={styles.storePanel}
+            style={{ height: !buildStore ? "25rem" : "fit-content" }}
+          >
             <h1>Loja</h1>
-            <Link to="/createStore" className={styles.link}>
-              <button className={styles.start}>Começar?</button>
-            </Link>
+            {!buildStore ? (
+              <>
+                <button
+                  className={styles.start}
+                  onClick={() => setBuildStore(true)}
+                >
+                  Começar?
+                </button>
+              </>
+            ) : (
+              <>
+                <Form
+                  className={styles.Form}
+                  errors={errors}
+                  onSubmit={handleSubmit}
+                >
+                  <Field.Root name="title" className={styles.Field}>
+                    <Field.Label className={styles.Label}>
+                      Nome da loja
+                    </Field.Label>
+                    <Field.Control
+                      type="text"
+                      required
+                      placeholder="Minha loja 123"
+                      className={styles.Input}
+                    />
+                    <Field.Error className={styles.Error} />
+                  </Field.Root>
+
+                  <Field.Root name="description" className={styles.Field}>
+                    <Field.Label className={styles.Label}>
+                      Descrição da loja
+                    </Field.Label>
+                    <Field.Control
+                      type="text"
+                      required
+                      placeholder="Compre produtos incríveis!"
+                      className={styles.Input}
+                    />
+                    <Field.Error className={styles.Error} />
+                  </Field.Root>
+                  <Field.Root name="thumbnail" className={styles.Field}>
+                    <Field.Label className={styles.Label}>
+                      Imagem da loja
+                    </Field.Label>
+                    <Field.Control
+                      type="url"
+                      required
+                      placeholder="https://minhaloja.com/imagem.png"
+                      className={styles.Input}
+                    />
+                    <Field.Error className={styles.Error} />
+                  </Field.Root>
+
+                  <Button
+                    type="submit"
+                    disabled={loading}
+                    focusableWhenDisabled
+                    className={styles.Button}
+                  >
+                    Submit
+                  </Button>
+
+                  <Button
+                    className={styles.Button}
+                    onClick={() => {
+                      setBuildStore(false);
+                    }}
+                  >
+                    Cancelar
+                  </Button>
+                </Form>
+              </>
+            )}
           </div>
         </div>
       </div>

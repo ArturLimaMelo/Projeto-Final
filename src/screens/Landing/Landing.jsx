4e4f6c2afb2465dd28b361cbecb89a3bd6c021c7
NEGRESC0 <156157000+NEGRESC0@users.noreply.github.com>
2026-01-22 14:55:43 -0300

    landing page

--- src/screens/Landing/Landing.jsx
+++ src/screens/Landing/Landing.jsx
@@ -1,23 +1,18 @@
 import styles from "./Landing.module.css";
-import CircularProgress from '@mui/joy/CircularProgress';
-
+import CircularProgress from "@mui/joy/CircularProgress";
+import Spline from "@splinetool/react-spline";
 
 export default function Landing() {
   return (
     <>
-      <h1 className={styles.title}>O Futuro do seu negócio</h1>
-      <p className={styles.subtitle}>
-        Impulsione suas vendas e expanda seu alcance.
-      </p>
-      <button className={styles.start}>
-        Comece Agora
-        <CircularProgress
-          color="primary"
-          determinate={false}
-          size="sm"
-          variant="plain"
-        />
-      </button>
+         <Spline scene="https://prod.spline.design/nhe4VadwsE4vtqiX/scene.splinecode" />
     </>
   );
 }
+
+/* <CircularProgress
+  color="primary"
+  determinate={false}
+  size="sm"
+  variant="plain"
+/> */

a3be4fa724fa9eb09c5bb1a5bd7cce6c6f0d59d2
NEGRESC0 <156157000+NEGRESC0@users.noreply.github.com>
2026-01-22 17:13:26 -0300

    landing page mobile adaptation

--- src/screens/Landing/Landing.jsx
+++ src/screens/Landing/Landing.jsx
@@ -1,13 +1,28 @@
+// Importa o CSS Module específico dessa página
 import styles from "./Landing.module.css";
+
+// Importa um loader circular do MUI (não está sendo usado ainda)
 import CircularProgress from "@mui/joy/CircularProgress";
+
+// Importa o componente oficial do Spline para React
 import Spline from "@splinetool/react-spline";
 
+// Componente principal da Landing Page
 export default function Landing() {
   return (
-    <>   
+    // Section que envolve o Spline
+    // Serve como "janela de recorte" (viewport do 3D)
     <section className={styles.splineWrapper}>
-         <Spline scene="https://prod.spline.design/nhe4VadwsE4vtqiX/scene.splinecode" />
+
+      {/* 
+        Componente do Spline
+        Ele renderiza internamente:
+        <div>
+          <canvas />
+        </div>
+      */}
+      <Spline scene="https://prod.spline.design/nhe4VadwsE4vtqiX/scene.splinecode" />
+
     </section>
-    </>
   );
 }
